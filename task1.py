ba17f6d1ba2fb898426e787c5459d4591ed36ef5
Reza Safdari <arezoofarmani66@gmail.com>
2022-11-04 16:41:14 +0330

    first commit

---
+++ task1.py
@@ -0,0 +1,13 @@
+if __name__=="__main__":
+    my_list = [25, 79, 86, 120, 79, 456, 58, 79, 86, 25]
+
+    repeat_dict = dict()
+    for index, number in enumerate(my_list):
+        if number in repeat_dict.keys():
+            repeat_dict[number].append(index)
+        else:
+            repeat_dict[number] = [index]
+
+for key, value in repeat_dict.items():
+    if len(value) > 1:
+        print(f"Number {key} was found {len(value)} times at indices: {value}")
---
+++ task2.py
@@ -0,0 +1,16 @@
+if __name__ == "__main__":
+    my_list = [8, 10, 12, 13, 20, 25, 26, 33, 37, 38, 44, 53]
+    target_sum = 45
+
+    front = 0
+    last = len(my_list) - 1
+    while front <= last:
+        sum = my_list[front] + my_list[last]
+        if sum == target_sum:
+            print(f"{my_list[front]} + {my_list[last]} = {target_sum}")
+            last -= 1  # or front += 1
+        elif sum > target_sum:
+            last -= 1
+        elif sum < target_sum:
+            front += 1
+
